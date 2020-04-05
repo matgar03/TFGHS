dd065dd30c3dbb804ef927c7423fc4179ee5a901
jorge <jsainero@ucm.es>
2020-04-05 13:51:17 +0200

    casi hecho el MCTS2, falta tener un poco más en cuenta los END_TURN y probarlo

---
+++ HearthstoneAICompetition-master/core-extensions/SabberStoneCoreAi/src/Agent/MCTS2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SabberStoneCore.Tasks.PlayerTasks;
+using SabberStoneCoreAi.Agent;
+using SabberStoneCoreAi.POGame;
+
+namespace SabberStoneCoreAi.src.Agent
+{
+	class MCTS2 : AbstractAgent
+	{
+
+		int nsimulations = 1000;
+		Nodo2 root;
+		public override void FinalizeAgent()
+		{
+			
+		}
+
+		public override void FinalizeGame()
+		{
+			
+		}
+
+		public override PlayerTask GetMove(POGame.POGame poGame)
+		{
+			root = new Nodo2(poGame, null, null);
+			for (int i = 0; i < nsimulations; ++i)
+			{
+				Nodo2 selNode = Selection(root);
+				//el nodo devuelto debería ser expandible
+				Nodo2 expNode = selNode.expand();
+				int result = Nodo2.Simulate(expNode.getState());
+				Nodo2.BackPropagation(expNode, result);
+			}
+			return null;
+		}
+
+		public override void InitializeAgent()
+		{
+			
+		}
+
+		public override void InitializeGame()
+		{
+			
+		}
+
+		private Nodo2 Selection(Nodo2 node)
+		{
+			//si no han sido expandidos todos sus hijos este es el nodo seleccionado
+			if (!node.isExpanded())
+				return node;
+			//si han sido expandidos entonces seleccionamos el mejor hijo y delegamos la selección a él
+			else
+			{
+				return Selection(node.bestChild());
+			}
+		}
+
+	}
+}
---
+++ HearthstoneAICompetition-master/core-extensions/SabberStoneCoreAi/src/Agent/Nodo2.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SabberStoneCore.Tasks.PlayerTasks;
+using SabberStoneCoreAi.Agent;
+using SabberStoneCoreAi.Score;
+
+namespace SabberStoneCoreAi.src.Agent
+{
+	class Nodo2
+	{
+		Nodo2 padre;
+		PlayerTask task;
+		POGame.POGame state;
+		//las PlayerTask de hijos y posibles hijos tienen que ser disjuntas
+		//al principio la primera es vacía y la segunda contiene todas las acciones y cuando se expanden todos los hijos será al revés
+		List<Nodo2> hijos;
+		List<PlayerTask> posiblesHijos;
+
+		private float value;
+		private int visits;
+
+		public PlayerTask getTask() { return task; }
+
+		public POGame.POGame getState() { return state; }
+		public float getAverageValue() { return value / visits; }
+		public float getValue() { return value; }
+		public int getVisits() { return visits; }
+		public Nodo2(POGame.POGame state, Nodo2 padre, PlayerTask task)
+		{
+			this.state = state;
+			this.padre = padre;
+			this.task = task;
+			//hay que hacer la distinción para no pasar al turno del otro jugador
+			if (task.PlayerTaskType != PlayerTaskType.END_TURN)
+				posiblesHijos = state.CurrentPlayer.Options();
+			else posiblesHijos = new List<PlayerTask>();
+		}
+
+		public bool isExpanded()
+		{
+			return posiblesHijos.Count == 0;
+		}
+
+		public Nodo2 expand()
+		{
+			//seleccionamos aleatoriamente que hijo expandir
+			int i = Globals.r.Next(0, posiblesHijos.Count);
+			//simulamos el estado después de aplicarlo
+			POGame.POGame nextState = state.Simulate(new List<PlayerTask>() { posiblesHijos[i] })[posiblesHijos[i]];
+			//creamos el hijo expandido
+			Nodo2 expNode = new Nodo2(nextState, this, posiblesHijos[i]);
+			//lo añadimos al árbol
+			hijos.Add(expNode);
+			//lo eliminamos de los posibles hijos porque ya está en hijos
+			posiblesHijos.RemoveAt(i);
+			return expNode;
+		}
+		public Nodo2 bestChild()
+		{
+			hijos.Sort((x, y) => y.ucb().CompareTo(x.ucb()));
+			return hijos[0];
+		}
+		public float ucb()
+		{
+			return (float)(getAverageValue() + Globals.C * Math.Sqrt(Math.Log(padre.getVisits()) / visits));
+		}
+		private void addValue(float value)
+		{
+			++visits;
+			this.value += value;
+		}
+		private static int GetStateValue(POGame.POGame state)
+		{
+			//utilizamos al oponente porque evaluamos después de pasar turno
+			return new ScoreUtility { Controller = state.CurrentOpponent }.Rate();
+		}
+		public static int Simulate(POGame.POGame state)
+		{
+			List<PlayerTask> options = state.CurrentPlayer.Options();
+			int selectedOpt = Globals.r.Next(0, options.Count);
+			PlayerTask task = options[selectedOpt];
+			POGame.POGame nextState = state.Simulate(new List<PlayerTask>() { task })[task];
+			if (task.PlayerTaskType == PlayerTaskType.END_TURN)
+			{
+				return GetStateValue(nextState);
+			}
+			else return Simulate(nextState);
+		}
+
+		public static void BackPropagation(Nodo2 node, int result)
+		{
+			node.addValue(result);
+			if (node.padre != null)
+				BackPropagation(node.padre, result);
+		}
+	}
+}
