93717ee1d3c81c62ca116bc935fea57de27446b4
Mateo Garcia <matgar03@ucm.es>
2020-05-07 19:48:44 +0200

    Creacion mutacion

---
+++ HearthstoneAICompetition-master/core-extensions/SabberStoneCoreAi/src/Evolutivo/Mutacion.cs
@@ -0,0 +1,56 @@
+using SabberStoneCore.Config;
+using SabberStoneCore.Enums;
+using SabberStoneCoreAi.Agent;
+using SabberStoneCoreAi.Agent.ExampleAgents;
+using SabberStoneCoreAi.Meta;
+using SabberStoneCoreAi.POGame;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace SabberStoneCoreAi.src.Evolutivo
+{
+    static class Mutacion
+    {
+
+        public static Individuo totalAcotada(Individuo ind, double prob_mut)
+        {
+            Individuo res;
+            double[] punt = ind.getAttributes();
+            List<double> res_list = new List<double>();
+            for (int i = 0; i < punt.Length; ++i)
+            {
+                double cambio = Globals.r.NextDouble();
+                if (cambio <= prob_mut)
+                {
+                    double variacion = (Globals.r.NextDouble() - 0.5) / 5;
+                    res_list.Add(punt[i] + variacion);
+                }
+                else res_list.Add(punt[i]);
+
+            }
+            res = new Individuo(res_list);
+            return res;
+        }
+
+        public static Individuo aleatoriaNoCota(Individuo ind)
+        {
+            Individuo res;
+            double[] punt = ind.getAttributes();
+            List<double> res_list = new List<double>();
+            int pos = Globals.r.Next(punt.Length);
+            punt[pos] = Globals.r.NextDouble();
+
+            for (int i = 0; i < punt.Length; ++i)
+            {
+                res_list.Add(punt[i]);
+
+            }
+            res = new Individuo(res_list);
+            return res;
+        }
+
+
+    }
+}
